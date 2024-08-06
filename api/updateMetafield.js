b75008eb3d6e968f6399a023c40b19e564ab7550
Young-zai <48758181+Young-zai@users.noreply.github.com>
2024-08-06 15:07:35 +0800

    Update updateMetafield.js

--- api/updateMetafield.js
+++ api/updateMetafield.js
@@ -1,41 +1,30 @@
 import fetch from 'node-fetch';
 
-fetch('https://156e1b-51.myshopify.com/admin/api/2024-07/customers.json', {
-  method: 'POST',
-  headers: {
-    'X-Shopify-Access-Token': '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',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    'customer': {
-      'first_name': 'Steve',
-      'last_name': 'Lastnameson',
-      'email': 'steve.lastnameson@example.com',
-      'phone': '+15142546011',
-      'verified_email': true,
-      'addresses': [
-        {
-          'address1': '123 Oak St',
-          'city': 'Ottawa',
-          'province': 'ON',
-          'phone': '555-1212',
-          'zip': '123 ABC',
-          'last_name': 'Lastnameson',
-          'first_name': 'Mother',
-          'country': 'CA'
-        }
-      ],
-      'metafields': [
-            {
-                "namespace": "custom",
-                "key": "Whatsapp",
-                "value": "+15142546011",
-                "value_type": "string"
-            }
-      ],
-      'password': 'newpass',
-      'password_confirmation': 'newpass',
-      'send_email_welcome': false
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const customerData = req.body;
+
+    try {
+      const shopifyResponse = await fetch('https://156e1b-51.myshopify.com/admin/api/2024-07/customers.json', {
+        method: 'POST',
+        headers: {
+          'X-Shopify-Access-Token': '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',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(customerData)
+      });
+
+      const data = await shopifyResponse.json();
+
+      if (!shopifyResponse.ok) {
+        return res.status(shopifyResponse.status).json({ error: data });
+      }
+
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-  })
-});
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}

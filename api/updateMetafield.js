07ff6c4cf59a095c835e594416ad0174f28a4595
Young-zai <48758181+Young-zai@users.noreply.github.com>
2024-08-16 14:05:56 +0800

    Update updateMetafield.js

--- api/updateMetafield.js
+++ api/updateMetafield.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify(customerData)
       });
-
+      console.log(process.env.access_key)
       const data = await shopifyResponse.json();
 
       if (!shopifyResponse.ok) {
